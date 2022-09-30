3c1d72714b92c4d9691285e1d1a8a20a5a8e3072
vallenillaabouassie <vallenillaabouassie@educ.college-montreal.qc.ca>
2022-09-30 10:27:14 -0400

    commit final

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 
 import random
-#Prends un nombre au hasard entre 1 et 99
+
 number = random.randint(1, 99)
-#Demande a l'utilisateur son choix
+counter = 0
 guess = int(input("Entrez un nombre de 1 a 99: "))
 while number != "guess":
 
     if guess < number:
-        #si le nombre est plus petit que le nombre "number"
+        counter = counter + 1
         print("Votre nombre est trop bas")
-        #redemande une valeur
         guess = int(input("Entrez un nombre de 1 a 99: "))
     elif guess > number:
-        #si le nombre est plus grand que le nombre dans "Number"
-        print("Votre nombre est trop haut")
-        #redemande une valeur
+        counter = counter + 1
+        print("Votre nombre est trop grand")
         guess = int(input("Entrez un nombre de 1 a 99: "))
     else:
-        #si le nombre est correct
+        counter = counter + 1
         print("Vous l'avez eu!")
+        print(f"Ca vous a pris {counter} essais")
         break
